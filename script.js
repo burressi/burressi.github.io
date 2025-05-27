c11cf5248f64e98ad85dfb97cff7d126b1deb6a0
burressi <ian.n.burress@gmail.com>
2025-05-27 08:08:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ $(document).ready(function() {
     $(this).css('opacity', '1');
   });
 });
+document.querySelectorAll('.carousel').forEach(carousel => {
+  const images = carousel.querySelectorAll('.carouselImage');
+  let current = 0;
+
+  const showImage = index => {
+    images.forEach((img, i) => img.classList.toggle('active', i === index));
+  };
+
+  carousel.querySelector('.prev').addEventListener('click', () => {
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
+  });
+
+  carousel.querySelector('.next').addEventListener('click', () => {
+    current = (current + 1) % images.length;
+    showImage(current);
+  });
+
+  showImage(current);
+});
